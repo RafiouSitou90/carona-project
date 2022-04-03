7a7bd4006a63d1769cfd331c1e3e77171b1013cf
Rafiou Sitou <rafiousitou90@gmail.com>
2022-04-03 12:05:42 -0300

    feat(api): Initial commit.

---
+++ carona-api/src/main/java/com/iesb/gab/raf/carona/api/CaronaApiApplication.java
@@ -0,0 +1,13 @@
+package com.iesb.gab.raf.carona.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaronaApiApplication.class, args);
+    }
+
+}
---
+++ carona-api/src/test/java/com/iesb/gab/raf/carona/api/CaronaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesb.gab.raf.carona.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
